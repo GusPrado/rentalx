8b3f0f502317dfacb9587bef59c4b903fc3af714
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2022-08-21 19:46:44 -0300

    feat: Add category route

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 
+import { categoriesRoutes } from './routes/categories.routes';
+
 const app = express();
 const api_port = 3333;
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'App on!' });
-});
+app.use(express.json());
+
+app.use('/categories', categoriesRoutes);
 
 app.listen(api_port, () => console.log(`Server running on port: ${api_port}`));
