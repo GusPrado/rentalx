f27559f64445bc0a7fc68f40852002801f5ca0dd
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2022-08-23 20:35:51 -0300

    feat: Add categories list method

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,17 +2,22 @@ import { Router } from 'express';
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (req, res) => {
   const { name, description } = req.body;
-
-  const categoriesRepository = new CategoriesRepository();
 
   categoriesRepository.create({ name, description });
 
   console.log(categoriesRepository);
 
   return res.status(201).send();
+});
+
+categoriesRoutes.get('/', (req, res) => {
+  const categories = categoriesRepository.list();
+
+  return res.status(200).json(categories);
 });
 
 export { categoriesRoutes };
