519c282ae7245e15a79b085235f4865e827879ea
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2022-09-03 22:02:19 -0300

    feat: Add specifications repository

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import { categoriesRoutes } from './routes/categories.routes';
+import { specificationsRoutes } from './routes/specifications.routes';
 
 const app = express();
 const api_port = 3333;
@@ -8,5 +9,6 @@ const api_port = 3333;
 app.use(express.json());
 
 app.use('/categories', categoriesRoutes);
+app.use('/specifications', specificationsRoutes);
 
 app.listen(api_port, () => console.log(`Server running on port: ${api_port}`));
