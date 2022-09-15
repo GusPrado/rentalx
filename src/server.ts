2f60b664e7430bac94bf735fb34cdf7df235f2a7
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2022-09-14 21:52:02 -0300

    refactor: Reorg routes

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
 import express from 'express';
-
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationsRoutes } from './routes/specifications.routes';
+import { router } from './routes';
 
 const app = express();
 const api_port = 3333;
 
 app.use(express.json());
 
-app.use('/categories', categoriesRoutes);
-app.use('/specifications', specificationsRoutes);
+app.use(router);
 
 app.listen(api_port, () => console.log(`Server running on port: ${api_port}`));
